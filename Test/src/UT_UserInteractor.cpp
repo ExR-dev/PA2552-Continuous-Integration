9f29d486b086f6325cc809f14d7c80f9170cd498
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-03-11 13:15:48 +0100

    Create UT_UserInteractor.cpp

---
+++ Test/src/UT_UserInteractor.cpp
@@ -0,0 +1,12 @@
+#include <UserInteractor.hpp>
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(UserInteractorTest, ParseAdd)
+{
+	std::string userInputOp = "add";
+	std::string userInputNum1 = "1";
+	std::string userInputNum2 = "1";
+
+	// TODO: Assert that parsing the user input correctly identifies the operation and numbers
+}
